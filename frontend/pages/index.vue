e0cd1c64c9ec85bc4247115464f5e995dfebed65
Hooo1941 <jiahao1941@outlook.com>
2019-11-26 19:55:55 +0800

    mobile

---
+++ frontend/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<h1>loading...</h1>
+</template>
+
+<script>
+export default {
+layout: 'mobile',
+mounted() {
+    if (navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {
+      alert("手机端");
+      this.$router.replace('/mobile/index');
+    } else {
+      alert("pc端");
+      this.$router.replace('/desktop/index');
+    }
+  }
+}
+</script>
