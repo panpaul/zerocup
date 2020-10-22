96264c8a8e859a1394b461fb1f3f30971ea29c63
Hooo1941 <jiahao1941@outlook.com>
2020-10-22 09:50:14 +0800

    fix bugs

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,3 +1,8 @@
+/*
+ * @Date: 2020-10-17 20:05:10
+ * @LastEditors: QiuJhao
+ * @LastEditTime: 2020-10-22 09:49:02
+ */
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://47.100.88.98:45555/api/v1";
+axios.defaults.baseURL = "http://127.0.0.1:45555/api/v1";
--- frontend/plugins/element-ui.js
+++ frontend/plugins/element-ui.js
@@ -1,5 +1,10 @@
+/*
+ * @Date: 2020-10-17 20:05:10
+ * @LastEditors: QiuJhao
+ * @LastEditTime: 2020-10-22 09:49:54
+ */
 import Vue from 'vue'
 import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale/lang/zh-CN'
 
 Vue.use(Element, {locale});
